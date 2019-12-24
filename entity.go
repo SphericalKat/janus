f05d1d33712915aa992ea4796c36d4b75877e11a
ATechnoHazard <amolele@gmail.com>
2019-12-24 23:43:12 +0530

    Add godoc comment to account struct
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- entity.go
+++ entity.go
@@ -1,5 +1,6 @@
 package janus
 
+// Account represents a user account
 type Account struct {
 	OrganizationID uint   `json:"org_id" gorm:"primary_key"`
 	CacheKey       string `json:"key" gorm:"primary_key"`
